b044886c3d4e79343ce587a1f4e9a05fd3b78305
simlenodir <nodirsmailov0@gmail.com>
2022-06-28 11:56:28 +0300

    dsgasd

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./Components/Container";
+import "./main.css";
+export const App = () => {
+  return (
+    <>
+      <Container>
+        <h2 className="title">Never give up</h2>
+      </Container>
+    </>
+  );
+};
